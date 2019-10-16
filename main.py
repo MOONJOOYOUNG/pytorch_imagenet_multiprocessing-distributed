1f105cd1389d6d2af71a7dd7a3f74b891d222267
MOONJOO <answn3475@ds.seoultech.ac.kr>
2019-10-17 02:56:38 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,425 @@
+import util
+import argparse
+import os
+import random
+import shutil
+import time
+import warnings
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.multiprocessing as mp
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torchvision
+
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+                    choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet18)')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N',
+                    help='mini-batch size (default: 256), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--world-size', default=-1, type=int,
+                    help='number of nodes for distributed training')
+parser.add_argument('--rank', default=-1, type=int,
+                    help='node rank for distributed training')
+parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
+                    help='url used to set up distributed training')
+parser.add_argument('--dist-backend', default='nccl', type=str,
+                    help='distributed backend')
+parser.add_argument('--seed', default=None, type=int,
+                    help='seed for initializing training. ')
+parser.add_argument('--gpu', default=None, type=int,
+                    help='GPU id to use.')
+parser.add_argument('--multiprocessing-distributed', action='store_true',
+                    help='Use multi-processing distributed training to launch '
+                         'N processes per node, which has N GPUs. This is the '
+                         'fastest way to use PyTorch for either single node or '
+                         'multi node data parallel training')
+
+# Custom parser
+"""---------------------------------------- 파서 작성 ---------------------------------------- """
+parser.add_argument('--save_path',default='./res152_softmax1.0/', type=str, help='savepath')
+parser.add_argument('--gpu_count',default= 4, type=int, help='use gpu count')
+
+"""---------------------------------------------코드 실행---------------------------------------------------- """
+# python main.py -a resnet50 --dist-url 'tcp://127.0.0.1:2222' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 none --save_path './test/' --gpu_count 4
+"""--------------------------------------------------------------------------------------------------------- """
+
+best_acc1 = 0
+args = parser.parse_args()
+
+def main():
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
+
+    if args.gpu is not None:
+        warnings.warn('You have chosen a specific GPU. This will completely '
+                      'disable data parallelism.')
+
+    if args.dist_url == "env://" and args.world_size == -1:
+        args.world_size = int(os.environ["WORLD_SIZE"])
+
+    args.distributed = args.world_size > 1 or args.multiprocessing_distributed
+
+    """ used gpu count ex) 4 , all use : torch.cuda.device_count() """
+    ngpus_per_node = args.gpu_count
+    if args.multiprocessing_distributed:
+        # Since we have ngpus_per_node processes per node, the total world_size
+        # needs to be adjusted accordingly
+        args.world_size = ngpus_per_node * args.world_size
+        # Use torch.multiprocessing.spawn to launch distributed processes: the
+        # main_worker process function
+        mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
+    else:
+        # Simply call main_worker function
+        main_worker(args.gpu, ngpus_per_node, args)
+
+def _build_tensor(size, value=None):
+    if value is None:
+        value = size
+    return torch.FloatTensor(size, size, size).fill_(value)
+
+def main_worker(gpu, ngpus_per_node, args):
+    global best_acc1
+    args.gpu = gpu
+
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu_count))
+
+    if args.distributed:
+        if args.dist_url == "env://" and args.rank == -1:
+            args.rank = int(os.environ["RANK"])
+        if args.multiprocessing_distributed:
+            # For multiprocessing distributed training, rank needs to be the
+            # global rank among all the processes
+            args.rank = args.rank * ngpus_per_node + gpu
+        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                world_size=args.world_size, rank=args.rank)
+
+    """---------------------------------------- 모델, opimizer,loss 선언 ---------------------------------------- """
+    if args.pretrained:
+        print("=> using pre-trained model '{}'".format(args.arch))
+        model = models.__dict__[args.arch](pretrained=True)
+    else:
+        print("=> creating model '{}'".format(args.arch))
+        model = models.__dict__[args.arch]()
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda(args.gpu)
+    optimizer = torch.optim.SGD(model.parameters(), args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+    """--------------------------------------------------------------------------------------------------------- """
+
+    if args.distributed:
+        # For multiprocessing distributed, DistributedDataParallel constructor
+        # should always set the single device scope, otherwise,
+        # DistributedDataParallel will use all available devices.
+        if args.gpu is not None:
+            torch.cuda.set_device(args.gpu)
+            model.cuda(args.gpu)
+            # When using a single GPU per process and per
+            # DistributedDataParallel, we need to divide the batch size
+            # ourselves based on the total number of GPUs we have
+            args.batch_size = int(args.batch_size / ngpus_per_node)
+            args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
+            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+            num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        else:
+            model.cuda()
+            # DistributedDataParallel will divide and allocate batch_size to all
+            # available GPUs if device_ids are not set
+            model = torch.nn.parallel.DistributedDataParallel(model)# output_device=0)
+    elif args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+    else:
+        # DataParallel will divide and allocate batch_size to all available GPUs
+        if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
+            model.features = torch.nn.DataParallel(model.features)
+            model.cuda()
+        else:
+            model = torch.nn.DataParallel(model).cuda()
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            if args.gpu is None:
+                checkpoint = torch.load(args.resume)
+            else:
+                # Map model to be loaded to specified single gpu.
+                loc = 'cuda:{}'.format(args.gpu)
+                checkpoint = torch.load(args.resume, map_location=loc)
+            args.start_epoch = checkpoint['epoch']
+            best_acc1 = checkpoint['best_acc1']
+            if args.gpu is not None:
+                # best_acc1 may be from a checkpoint from a different GPU
+                best_acc1 = best_acc1.to(args.gpu)
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    cudnn.benchmark = True
+
+
+    """---------------------------------------- 데이터 및 train 코드 작성. ----------------------------------------------- """
+
+    traindir = os.path.join('/data_ssd/public_dataset/imagenet/', 'train')
+    valdir = os.path.join('/data_ssd/public_dataset/imagenet/', 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    train_dataset = datasets.ImageFolder(
+        traindir,
+        transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+
+    ''' *지우지 말것*   train dataset 넘겨줄것. -> DistributedSampler(train_dataset) '''
+    if args.distributed:
+        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    else:
+        train_sampler = None
+    '''----------------------------------------------------------------------------'''
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+        num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    if args.evaluate:
+        validate(val_loader, model, criterion, args)
+        return
+
+    ''' -------------------------저장 경로 설정-----------------------------'''
+    save_path = args.save_path
+    if dist.get_rank() == 0:
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+    ''' -------------------------logger 선언----------------------------'''
+    train_logger = util.Logger(os.path.join(save_path, 'train.log'))
+    valid_logger = util.Logger(os.path.join(save_path, 'valid.log'))
+    train_time_logger = util.Logger(os.path.join(save_path, 'train_time.log'))
+    valid_time_logger = util.Logger(os.path.join(save_path, 'valid_time.log'))
+
+    ''' -------------------------학습 시작-----------------------------'''
+    for epoch in range(args.start_epoch, args.epochs):
+        ''' -------------------------건들지 말기.-----------------------------'''
+        if args.distributed:
+            train_sampler.set_epoch(epoch)
+        ''' -----------------------------------------------------------------'''
+
+        ''' -------------------------learning_rate decay---------------------'''
+        adjust_learning_rate(optimizer, epoch, args)
+
+        ''' -------------------------train-----------------------------'''
+        train(train_loader, model, criterion, optimizer, epoch, args, train_logger,train_time_logger)
+
+        ''' -------------------------valid-----------------------------'''
+        acc1 = validate(val_loader, model, criterion, epoch, args,valid_logger, valid_time_logger)
+
+        ''' -------------------------save checkpoint.-----------------------------'''
+        is_best = acc1 > best_acc1
+        best_acc1 = max(acc1, best_acc1)
+
+        if not args.multiprocessing_distributed or (args.multiprocessing_distributed
+                and args.rank % ngpus_per_node == 0):
+            util.save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': args.arch,
+                'state_dict': model.state_dict(),
+                'best_acc1': best_acc1,
+                'optimizer' : optimizer.state_dict(),
+            }, is_best)
+
+
+def train(train_loader, model, criterion, optimizer, epoch, args, logger, time_logger):
+    ''' -------------------------averageMeter 선언.-----------------------------'''
+    batch_time = util.AverageMeter('Time', ':6.3f')
+    data_time = util.AverageMeter('Data', ':6.3f')
+    losses = util.AverageMeter('Loss', ':.4f')
+    top1 = util.AverageMeter('Acc@1', ':6.2f')
+    top5 = util.AverageMeter('Acc@5', ':6.2f')
+
+    ''' -------------------------출력 progress 선언.-----------------------------'''
+    progress = util.ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix="Epoch: [{}]".format(epoch))
+
+    ''' -------------------------학습 시작.-----------------------------'''
+    model.train()
+    end = time.time()
+    for i, (images, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        if args.gpu is not None:
+            images = images.cuda(args.gpu, non_blocking=True)
+        target = target.cuda(args.gpu, non_blocking=True)
+
+
+        output = model(images)
+        loss = criterion(output, target)
+
+        ''' -------------------------이미지넷 top1, top5 accuracy----------------------------'''
+        acc1, acc5, correct = util.accuracy(output, target, topk=(1, 5))
+
+        ''' -------------------------각 GPU log 합쳐주기-----------------------------'''
+        reduced_loss = reduce_tensor(loss.data)
+        reduced_top1 = reduce_tensor(acc1[0].data)
+        reduced_top5 = reduce_tensor(acc5[0].data)
+
+        ''' ------------------------- averageMeter에 업데이트 -----------------------------'''
+        losses.update(reduced_loss.item(), images.size(0))
+        top1.update(reduced_top1.item(), images.size(0))
+        top5.update(reduced_top5.item(), images.size(0))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        ''' ------------------------- gpu 하나로만 출력하기. (rank == 0 : 0번 gpu에서만 출력하도록.)-----------------------------'''
+        if dist.get_rank() == 0:
+            if i % args.print_freq == 0:
+                progress.display(i)
+
+    ''' ------------------------- logger 에 업데이트-----------------------------'''
+    logger.write([epoch, losses.avg, top1.avg, top5.avg])
+    time_logger.write([epoch, batch_time.avg, data_time.avg])
+
+
+def validate(val_loader, model, criterion, epoch, args, logger, time_logger):
+    batch_time = util.AverageMeter('Time', ':6.3f')
+    data_time = util.AverageMeter('Data', ':6.3f')
+    losses = util.AverageMeter('Loss', ':.4f')
+    top1 = util.AverageMeter('Acc@1', ':6.2f')
+    top5 = util.AverageMeter('Acc@5', ':6.2f')
+    progress = util.ProgressMeter(
+        len(val_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix='Test: ')
+
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in enumerate(val_loader):
+            data_time.update(time.time() - end)
+
+            if args.gpu is not None:
+                images = images.cuda(args.gpu, non_blocking=True)
+            target = target.cuda(args.gpu, non_blocking=True)
+
+            output = model(images)
+            loss = criterion(output, target)
+
+            acc1, acc5, correct = util.accuracy(output, target, topk=(1, 5))
+
+            reduced_loss = reduce_tensor(loss.data)
+            reduced_top1 = reduce_tensor(acc1[0].data)
+            reduced_top5 = reduce_tensor(acc5[0].data)
+
+            losses.update(reduced_loss.item(), images.size(0))
+            top1.update(reduced_top1.item(), images.size(0))
+            top5.update(reduced_top5.item(), images.size(0))
+
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if dist.get_rank() == 0:
+                if i % args.print_freq == 0:
+                    progress.display(i)
+
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'.format(top1=top1, top5=top5))
+
+    logger.write([epoch, losses.avg, top1.avg, top5.avg])
+    time_logger.write([epoch, batch_time.avg, data_time.avg])
+
+    return top1.avg
+
+'''-----------------------------GPU에 있는 각 데이터 평균 취하기------------------------'''
+def reduce_tensor(tensor):
+    rt = tensor.clone()
+    dist.all_reduce(rt, op=dist.reduce_op.SUM)
+    # gpu 갯수로 나눠줌.
+    rt /= args.gpu_count
+    return rt
+
+'''----------------------------learning_rate deacy 설정 (ex)30 epcoh 마다 decay)-------------------------'''
+def adjust_learning_rate(optimizer, epoch, args):
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+if __name__ == '__main__':
+    main()
---
+++ util.py
@@ -0,0 +1,130 @@
+import os
+import shutil
+import matplotlib
+matplotlib.use('agg')
+import pandas as pd
+import numpy as np
+import torch
+from collections import Iterable
+
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+class Logger(object):
+    def __init__(self, path, int_form=':04d', float_form=':.6f'):
+        self.path = path
+        self.int_form = int_form
+        self.float_form = float_form
+        self.width = 0
+
+    def __len__(self):
+        try: return len(self.read())
+        except: return 0
+
+    def write(self, values):
+        if not isinstance(values, Iterable):
+            values = [values]
+        if self.width == 0:
+            self.width = len(values)
+        assert self.width == len(values), 'Inconsistent number of items.'
+        line = ''
+        for v in values:
+            if isinstance(v, int):
+                line += '{{{}}} '.format(self.int_form).format(v)
+            elif isinstance(v, float):
+                line += '{{{}}} '.format(self.float_form).format(v)
+            elif isinstance(v, str):
+                line += '{} '.format(v)
+            else:
+                raise Exception('Not supported type.')
+        with open(self.path, 'a') as f:
+            f.write(line[:-1] + '\n')
+
+    def read(self):
+        with open(self.path, 'r') as f:
+            log = []
+            for line in f:
+                values = []
+                for v in line.split(' '):
+                    try:
+                        v = float(v)
+                    except:
+                        pass
+                    values.append(v)
+                log.append(values)
+        return log
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        _, re_pred = output.topk(1, 1, True, True)
+        re_pred = re_pred.t()
+        re_correct = re_pred.eq(target.view(1, -1).expand_as(re_pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res[0], res[1], re_correct.squeeze()
+
+def one_hot_encoding(data_y):
+    print("one_hot_encoding process")
+    cls = set(data_y)
+    class_dict = {c: np.identity(len(cls))[i, :] for i, c in enumerate(cls)}
+    one_hot = np.array(list(map(class_dict.get, data_y)))
+
+    return one_hot
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+def save_data(name, data, save_path):
+    df_data = pd.DataFrame(data)
+    df_data.to_csv(save_path + '{0}_data.csv'.format(name), encoding='ms949')
